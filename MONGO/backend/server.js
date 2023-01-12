03843a2ce83c4ac05bb2d6377d059cb8bb5fb6c4
Jarrett Guyer <guyer.jarrett@gmail.com>
2023-01-12 08:53:58 -0700

    finished mongo server

--- MONGO/backend/server.js
+++ MONGO/backend/server.js
@@ -26,31 +26,201 @@ app.use(express.urlencoded({ extended: true }));
 
 // ==================== Routes  ====================
 
-// Goals:
+/* ===== Collection Design =====
+  Users:
+   userID, name, userName, signup, propic
+  Gernes:
+   genreID, name
+  Artists:
+   artistID, name, portrait
+  Albums:
+   albumID, name, release, art, genre, artist
+  Songs:
+   songID, name, length, listens, album, artist, genre
+  Playlists:
+   playlistID, userID, songs, image
+*/
 
-// Search all tables
+// ===== Search Tables in various ways
 
-// Get X lines from all tables
+// Users =====
 
-// put post delete all tables (mongo makes this easier)
-
-// Get all from a table (woof)
-
-app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to ChronoBeats' });
-});
-
-app.get('/users/:count', (req, res) => {
-  User.find()
-    .limit(req.params.count)
+app.get('/users/name/:term/:count', (req, res) => {
+  User.find({ name: new RegExp(req.params.term, 'i') })
+    .limit(req.params.count ? req.params.count : 50)
     .then((result) => {
       res.send(result);
     })
     .catch((err) => res.status(404).send(err));
 });
 
-app.get('/users/search/:term', (req, res) => {
-  User.find({ name: new RegExp(req.params.term, 'i') })
+app.get('/users/newest/:count', (req, res) => {
+  User.find()
+    .sort({ signup: -1 })
+    .limit(req.params.count ? req.params.count : 50)
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => res.status(404).send(err));
+});
+
+app.get('/users/id/:id', (req, res) => {
+  User.find({ userID: req.params.id })
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => res.status(404).send(err));
+});
+
+// Artist =====
+
+app.get('/artists/name/:term/:count', (req, res) => {
+  Artist.find({ name: new RegExp(req.params.term, 'i') })
+    .limit(req.params.count ? req.params.count : 50)
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => res.status(404).send(err));
+});
+
+app.get('/artist/id/:id', (req, res) => {
+  Artist.find({ artistID: req.params.id })
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => res.status(404).send(err));
+});
+
+// Album =====
+
+app.get('/albums/name/:term/:count', (req, res) => {
+  Album.find({ name: new RegExp(req.params.term, 'i') })
+    .limit(req.params.count ? req.params.count : 50)
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => res.status(404).send(err));
+});
+
+app.get('/ablums/newest/:count', (req, res) => {
+  Album.find()
+    .sort({ release: -1 })
+    .limit(req.params.count ? req.params.count : 50)
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => res.status(404).send(err));
+});
+
+app.get('/albums/genre/:genre/', (req, res) => {
+  Album.find({ genre: req.params.genre })
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => res.status(404).send(err));
+});
+
+app.get('/albums/artist/:artist/', (req, res) => {
+  Album.find({ artist: req.params.artist })
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => res.status(404).send(err));
+});
+
+app.get('/albums/id/:id', (req, res) => {
+  Album.find({ albumID: req.params.id })
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => res.status(404).send(err));
+});
+
+// Song =====
+
+app.get('/songs/name/:term/:count', (req, res) => {
+  Song.find({ name: new RegExp(req.params.term, 'i') })
+    .limit(req.params.count ? req.params.count : 50)
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => res.status(404).send(err));
+});
+
+app.get('/songs/popular/:count', (req, res) => {
+  Song.find()
+    .sort({ listens: -1 })
+    .limit(req.params.count ? req.params.count : 50)
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => res.status(404).send(err));
+});
+
+app.get('/songs/album/:album/', (req, res) => {
+  Song.find({ album: req.params.album })
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => res.status(404).send(err));
+});
+
+app.get('/songs/artist/:artist/', (req, res) => {
+  Song.find({ artist: req.params.artist })
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => res.status(404).send(err));
+});
+
+app.get('/songs/genre/:genre/', (req, res) => {
+  Song.find({ genre: req.params.genre })
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => res.status(404).send(err));
+});
+
+app.get('/songs/id/:id', (req, res) => {
+  Song.find({ albumID: req.params.id })
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => res.status(404).send(err));
+});
+
+// Genre =====
+
+app.get('/genres/name/:term/:count', (req, res) => {
+  Genre.find({ name: new RegExp(req.params.term, 'i') })
+    .limit(req.params.count ? req.params.count : 50)
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => res.status(404).send(err));
+});
+
+app.get('/genres/id/:id', (req, res) => {
+  Genre.find({ genreID: req.params.id })
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => res.status(404).send(err));
+});
+
+// Playlist ======
+
+app.get('/playlists/user/:userID/', (req, res) => {
+  Playlist.find({ userID: req.params.userID })
+    .limit(req.params.count ? req.params.count : 50)
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => res.status(404).send(err));
+});
+
+app.get('/playlists/id/:id', (req, res) => {
+  Playlist.find({ playlistID: req.params.id })
     .then((result) => {
       res.send(result);
     })
